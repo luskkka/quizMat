01f459fa3c78a22b9aa30ec172569cf826a5ff09
luskkka <50881743+luskkka@users.noreply.github.com>
2019-05-21 13:43:08 -0300

    adicionar projeto

---
+++ questions.js
@@ -0,0 +1,24 @@
+var questions = [
+
+    ["(Enem 2015)  O gerente de um cinema fornece anualmente ingressos gratuitos para escolas. Este ano, serão distribuídos   ingressos para uma sessão vespertina e   ingressos para uma sessão noturna de um mesmo filme. Várias escolas podem ser escolhidas para receberem ingressos. Há alguns critérios para a distribuição dos ingressos:",
+        "2", "4", "9", "40","80",
+        "img/ex1.jpg", "", "", "", "","",
+        "3"
+    ],
+
+    ["(Enem 2015)  Um arquiteto está reformando uma casa. De modo a contribuir com o meio ambiente, decide reaproveitar tábuas de madeira retiradas da casa. Ele dispõe de   tábuas de     de   e   de   todas de mesma largura e espessura. Ele pediu a um carpinteiro que cortasse as tábuas em pedaços de mesmo comprimento, sem deixar sobras, e de modo que as novas peças ficassem com o maior tamanho possível, mas de comprimento menor que  Atendendo ao pedido do arquiteto, o carpinteiro deverá produzir ",
+        "105 peças", "120 peças", "210 peças", "243 peças","420 peças",
+        "", "", "", "", "","",
+        "3"
+    ],
+    ["(Enem 2015)  Uma carga de   contêineres, idênticos ao modelo apresentado na Figura 1, devera ser descarregada no porto de uma cidade. Para isso, uma área retangular de   por   foi cedida para o empilhamento desses contêineres (Figura 2). ",
+        "105 peças", "120 peças", "210 peças", "243 peças","420 peças",
+        "img/ex3.jpg", "", "", "", "","",
+        "3"
+    ],
+    ["(Enem 2015)  Uma carga de   contêineres, idênticos ao modelo apresentado na Figura 1, devera ser descarregada no porto de uma cidade. Para isso, uma área retangular de   por   foi cedida para o empilhamento desses contêineres (Figura 2). ",
+        "12,5 m.", "17,5 m", "25 m", "22,5 m","32,5 m",
+        "img/ex3.jpg", "", "", "", "","",
+        "3"
+    ],
+]
